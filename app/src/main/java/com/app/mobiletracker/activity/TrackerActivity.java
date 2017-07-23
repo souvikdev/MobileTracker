7f99b1dae24c82cdfa2eb5efe9ee946b4409fa53
souvik dasgupta <sdasgupta3@sapient.com>
2017-07-23 11:53:01 +0530

    Code commit-Update activity

--- app/src/main/java/com/app/mobiletracker/activity/TrackerActivity.java
+++ app/src/main/java/com/app/mobiletracker/activity/TrackerActivity.java
@@ -2,6 +2,9 @@ package com.app.mobiletracker.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton;
 
 import com.app.mobiletracker.R;
 
@@ -11,5 +14,16 @@ public class TrackerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.permToggle);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(getApplicationContext(),"Switched On",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Switched Off",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
